b77f766097b090d763aa3207d2e7e088f51d0310
Princes Ruth <princesruth.enacinares@gmail.com>
2021-06-04 22:36:55 +0800

    first commit

--- PrincesList/urls.py
+++ PrincesList/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
 '''urlpatterns = [    
     url(r'^$', views.home_page, name='home_page'),    
     url(r'^PList/new$', views.new_list, name='new_list'),    
+    path('admin/', admin.site.urls),
+    path('', include('PList.urls')),
     url(r'^PList/(\d+)/$', views.view_list, name='view_list'),    
     url(r'^PList/(\d+)/add_item$', views.add_item, name='add_item')]'''

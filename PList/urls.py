b2162c07640787e56a789ca85804d40c3744b5b7
Princes Ruth <princesruth.enacinares@gmail.com>
2021-06-04 10:31:03 +0800

    deployment

--- PList/urls.py
+++ PList/urls.py
@@ -1,12 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url
+from PList import views
 #from django.urls import path
 #from django.http import HttpResponse
-urlpatterns = [
-	path('admin/', admin.site.urls),
-	path('', include('PList.urls')),
-]
-'''urlpatterns = [    
+
+urlpatterns = [    
     url(r'^$', views.home_page, name='home_page'),    
     url(r'^PList/new$', views.new_list, name='new_list'),    
     url(r'^PList/(\d+)/$', views.view_list, name='view_list'),    
--- PrincesList/wsgi.py
+++ PrincesList/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PrincesList.settings')
+sys.path.append('path/to/yourprojectenv/lib/python3.8/site-packages')
 
 application = get_wsgi_application()

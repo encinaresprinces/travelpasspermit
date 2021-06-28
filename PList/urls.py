cf736f62606ca3db5edfdd281f1a9577ec8fc6ba
Princes Ruth <princesruth.enacinares@gmail.com>
2021-06-28 15:56:28 +0800

    finals

--- PList/urls.py
+++ PList/urls.py
@@ -6,12 +6,33 @@ from PList import views
 
 urlpatterns = [
 	url(r'^$', views.home_page, name='home_page'),
-	url('Login/', views.login, name='login'),
-	url('Sgnup/', views.signup1, name='sgnup'),
-	url('signup/', views.signup2, name='signup'),
-	url('ssup/', views.signup3, name='ssup'),
-	url('ssupp/', views.signup4, name='ssupp'),
-]
+	
+
+	url(r'^new$', views.new_item, name='new_item'),
+    url(r'^(\d+)/view_list$', views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+
+    url(r'^edit/(?P<id>\d+)$', views.edit, name='edit'),
+    url(r'^edit/update/(?P<id>\d+)$', views.update, name='update'),
+	url(r'^signup2$', views.signup2, name='signup2'),
+	url(r'^signup3$', views.signup3, name='signup3'),
+	url(r'^signup4$', views.signup4, name='signup4'),
+
+    url(r'^delete/(?P<id>\d+)$', views.delete, name='delete'),
+    ]
+
+	
+
+
+
+'''url(r'Login/', views.login, name='login'),
+	url(r'Sgnup/', views.sgnup, name='sgnup'),
+	url(r'Signup//', views.signup, name='signup'),
+	url(r'ssup/', views.ssup, name='ssup'),
+	url(r'ssupp/', views.ssupp, name='ssupp'),'''
+
+
+
 
 
 
--- PList/views.py
+++ PList/views.py
@@ -1,11 +1,73 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from PList.models import  Barangay, TravelerProfile
-
+from django.shortcuts import redirect, render
+from PList.models import Barangay, TravelerProfile, Trequest, Requirements, Status
+from django.views.decorators.csrf import csrf_exempt
 
 def home_page(request):
+    barangays = Barangay.objects.all()
+    return render(request, 'Login.html',{'barangays': barangays})
+
+def new_item(request):
+    barangayt_= Barangay.objects.create(zbarangay = request.POST['TBarangay'],municipal = request.POST['Amunicipal'],address = request.POST['Oaddress'])
+    return redirect(f'/{barangayt_.id}/view_list')
+
+def view_list(request,barangay_id):
+    barangay_= Barangay.objects.get(id = barangay_id)
+    #barangay = Tname.objects.filter(barangay = barangay_id)
+    return render(request,'Sgnup.html', {'barangay': barangay_})
+
+def add_item(request,barangay_id):
+    barangay_= Barangay.objects.get(id= barangay_id)
+    TravelerProfile.objects.create (Tname=request.POST['lname'],birthday = request.POST['Fbirthday'],gender = request.POST['Gender'],address = request.POST['Faddress'],cnumber = request.POST['ccnumber'],email = request.POST['eaddress'],barangay = barangay_)
+    return redirect ('/signup2')
+
+def edit(request, id):
+    barangays = Barangay.objects.get(id=id)
+    context = {'barangays':barangays}
+    return render(request, 'ssuppp.html', context)
+
+def update(request, id):
+    barangay = Barangay.objects.get(id=id)
+    barangay.zbarangay =request.POST['TBarangay']
+    barangay.municipal =request.POST['Amunicipal']
+    barangay.address=request.POST['Oaddress']
+    barangay.save()
+    return redirect('/')
+
+def delete(request, id):
+
+    barangay = Barangay.objects.get(id=id)
+    barangay.delete()
+    return redirect('/')
+
+
+def signup2(request):
+
+
+    return render(request, 'Signup.html')
+
+
+def signup3(request):
+
+    return render(request, 'ssup.html')
+
+def signup4(request):
+
+    return render(request, 'ssupp.html')
+
+
+
+
+
+
+
+
+
+
+
+'''def home_page(request):
     barangays = Barangay.objects.all() 
     return render(request, 'home.html',{'barangays' : barangays })
+def new
 
 def login(request):
     #login_ = Barangay.objects.get(id=barangay_id)
@@ -18,6 +80,7 @@ def signup1(request):
 
 def signup2(request):
 
+
     return render(request, 'Signup.html')
 
 
@@ -27,7 +90,7 @@ def signup3(request):
 
 def signup4(request):
 
-    return render(request, 'ssupp.html')
+    return render(request, 'ssupp.html')'''
 
 
 
